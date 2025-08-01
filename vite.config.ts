f9722003940b3cbdc9db505b4bda52488c7dac2c
Marciano Tom <tom@yourfairbid.com>
2025-08-01 19:00:36 +0530

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/getreddconsulting/", // ✅ matches the repo name on GitHub
+  base: "/", // ✅ matches the repo name on GitHub
   plugins: [react()],
 });
